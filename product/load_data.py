f9d25421658748ea08c3add23c504eada8a5eb93
Abdulsalam Ali <abdulsalamali1997@gmail.com>
2026-02-11 13:24:00 +0300

    Update load_data.py

--- product/load_data.py
+++ product/load_data.py
@@ -3,22 +3,23 @@ import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
 
-SCOPES = [
-    "https://www.googleapis.com/auth/spreadsheets.readonly",
-    "https://www.googleapis.com/auth/drive.readonly",
-]
 
-@st.cache_data
+@st.cache_data(ttl=300)  # Auto refresh every 5 minutes (300 seconds)
 def load_data():
-    credentials = Credentials.from_service_account_info(
+    scope = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive",
+    ]
+
+    creds = Credentials.from_service_account_info(
         st.secrets["service_account"],
-        scopes=SCOPES
+        scopes=scope,
     )
 
-    client = gspread.authorize(credentials)
+    client = gspread.authorize(creds)
 
-    # sheet_id is INSIDE [service_account] in your toml
     sheet_id = st.secrets["service_account"]["sheet_id"]
-
     sheet = client.open_by_key(sheet_id).sheet1
-    return pd.DataFrame(sheet.get_all_records())
+
+    data = sheet.get_all_records()
+    return pd.DataFrame(data)
