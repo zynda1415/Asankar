3da938fb5ac4b0e4d3aac632f0016007cc4546a5
Abdulsalam Ali <abdulsalamali1997@gmail.com>
2026-02-15 17:01:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,12 @@ import streamlit as st
 from price_calculator import show_price_calculator
 from product_view import show_products_page
 
-st.set_page_config(
-    page_title="Asankar Factory",
-    page_icon="🏭",
-    layout="wide"
-)
-
-st.markdown('<meta http-equiv="refresh" content="3600">', unsafe_allow_html=True)
+st.set_page_config(page_title="Asankar Factory", layout="wide")
 
 WHATSAPP_PHONE = "9647501003839"
 
 with st.sidebar:
-    st.image("logo.png", width=170)
-    page = st.radio(
-        "Navigation",
-        ["Products", "Price Calculator"],
-        label_visibility="collapsed"
-    )
+    page = st.radio("", ["Products", "Price Calculator"])
 
 if page == "Products":
     show_products_page(WHATSAPP_PHONE)
