d02743439b34285271afaeba6b1d4d9c7ba24f2a
Abdulsalam Ali <abdulsalamali1997@gmail.com>
2026-02-15 16:38:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 import streamlit as st
 from price_calculator import show_price_calculator
-from contactus import show_contact
-from aboutas import show_about
 from product_view import show_products_page
 
-st.set_page_config(page_title="Asankar Factory", page_icon="🏭", layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(
+    page_title="Asankar Factory",
+    page_icon="🏭",
+    layout="wide"
+)
+
 st.markdown('<meta http-equiv="refresh" content="3600">', unsafe_allow_html=True)
 
 WHATSAPP_PHONE = "9647501003839"
 
 with st.sidebar:
     st.image("logo.png", width=170)
-    st.markdown("### Navigation")
-    page = st.radio("", ["Products", "Price Calculator", "Contact Us", "About Us"], label_visibility="collapsed")
+    page = st.radio(
+        "Navigation",
+        ["Products", "Price Calculator"],
+        label_visibility="collapsed"
+    )
 
 if page == "Products":
     show_products_page(WHATSAPP_PHONE)
+
 elif page == "Price Calculator":
     show_price_calculator()
-elif page == "Contact Us":
-    show_contact(phone=WHATSAPP_PHONE)
-elif page == "About Us":
-    show_about()
