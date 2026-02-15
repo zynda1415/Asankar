ae61293eca0f346acb8d99f40f03d3ff2cc458df
Abdulsalam Ali <abdulsalamali1997@gmail.com>
2026-02-15 16:58:20 +0300

    Update load_data.py

--- product/load_data.py
+++ product/load_data.py
@@ -6,8 +6,12 @@ from google.oauth2.service_account import Credentials
 def load_data():
     creds = Credentials.from_service_account_info(
         st.secrets["gcp_service_account"],
-        scopes=["https://www.googleapis.com/auth/spreadsheets.readonly"]
+        scopes=[
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive"
+        ]
     )
+
     client = gspread.authorize(creds)
     sheet = client.open("Your Sheet Name").sheet1
     data = sheet.get_all_records()
