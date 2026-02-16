d3fd6d08cc81b367b46500aa790855f19d1fae94
Abdulsalam Ali <abdulsalamali1997@gmail.com>
2026-02-16 14:36:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
 import streamlit as st
-from price_calculator import show_price_calculator
 from product_view import show_products_page
+from price_calculator import show_price_calculator
+from contactus import show_contact
 
-st.set_page_config(page_title="Asankar Factory", layout="wide")
+st.set_page_config(
+    page_title="Asankar Factory",
+    page_icon="🏭",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+# Auto refresh every 1 hour
+st.markdown('<meta http-equiv="refresh" content="3600">', unsafe_allow_html=True)
 
 WHATSAPP_PHONE = "9647501003839"
 
 with st.sidebar:
-    page = st.radio("", ["Products", "Price Calculator"])
+    st.image("logo.png", width=170)
+    page = st.radio(
+        "",
+        ["Products", "Price Calculator", "Contact Us"],
+        label_visibility="collapsed"
+    )
 
 if page == "Products":
     show_products_page(WHATSAPP_PHONE)
 
 elif page == "Price Calculator":
     show_price_calculator()
+
+elif page == "Contact Us":
+    show_contact(phone=WHATSAPP_PHONE)
