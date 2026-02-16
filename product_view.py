e5ceba779a642130a3c155bf77914abf41b29318
Abdulsalam Ali <abdulsalamali1997@gmail.com>
2026-02-16 14:37:01 +0300

    Update product_view.py

--- product_view.py
+++ product_view.py
@@ -3,7 +3,8 @@ from product.load_data import load_data
 from product.product_grid import show_product_grid
 
 def show_products_page(whatsapp_phone):
-    st.markdown("<h2 style='margin-top:0;'>بەرهەمەکانمان</h2>", unsafe_allow_html=True)
+    st.markdown("<h2>بەرهەمەکانمان</h2>", unsafe_allow_html=True)
+
     df = load_data()
 
     if df.empty:
