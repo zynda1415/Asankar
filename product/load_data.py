3e21c91594b515c2526a9f244c7208eaa3d2badb
Abdulsalam Ali <abdulsalamali1997@gmail.com>
2026-02-16 14:36:39 +0300

    Update load_data.py

--- product/load_data.py
+++ product/load_data.py
@@ -1,18 +1,18 @@
 import streamlit as st
 import gspread
-import pandas as pd
 from google.oauth2.service_account import Credentials
+import pandas as pd
 
+SHEET_ID = "1I9X2swZuDwxuuRlUituBojqKmwjomVr-K9kLeTWjtUU"
+SHEET_NAME = "asankar_product_images"
+
+@st.cache_data(ttl=60)
 def load_data():
-    creds = Credentials.from_service_account_info(
+    credentials = Credentials.from_service_account_info(
         st.secrets["gcp_service_account"],
-        scopes=[
-            "https://www.googleapis.com/auth/spreadsheets",
-            "https://www.googleapis.com/auth/drive"
-        ]
+        scopes=["https://www.googleapis.com/auth/spreadsheets.readonly"]
     )
-
-    client = gspread.authorize(creds)
-    sheet = client.open("asankar_product_images").sheet1
+    client = gspread.authorize(credentials)
+    sheet = client.open_by_key(SHEET_ID).worksheet(SHEET_NAME)
     data = sheet.get_all_records()
     return pd.DataFrame(data)
